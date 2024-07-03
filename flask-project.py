56e05cd579958cacbd887370676917338a7e2d14
sheshikumar1 <sheshikumarerraboina@gmail.com>
2024-07-03 23:16:00 +0530

    first commit

---
+++ flask-project.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,url_for
+from form import RegistrationForm,LoginForm
+app = Flask(__name__)
+app.config['SECRET_KEY']='e0ffa31463abb69d1c115d97d446334a'
+posts=[
+       {
+        'author':'Sheshi kumar',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'june 18,2024'},
+       {
+        'author':'nitin',
+        'title':'Blog Post 2',
+        'content':'second post content',
+        'date_posted':'june 19,2024'}
+       ]
+
+
+
+@app.route("/")
+@app.route("/home")
+def home_page():
+    return render_template('index.html',posts=posts)
+    
+@app.route("/about")
+def about():
+    return render_template('about.html',title='About')
+
+@app.route('/register' ,methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    return render_template('register.html',title='Register',form=form)
+    
+
+@app.route('/login')
+def login():
+    form=LoginForm()
+    return render_template('login.html',title='Login',form=form)
+        
+
+if __name__=="__main__":
+    app.run(debug=True)
